33985dc36256d84308ca6cb1a0bc79992a41a376
kamangir <arash@kamangir.net>
2022-08-13 18:43:19 -0700

    todo#147; setting up abcli

---
+++ abcli/rv22.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+function rv22() {
+    local task=$(abcli_unpack_keyword $1 help)
+
+    if [ $task == "help" ] ; then
+        abcli_help_line "rv22 task_1" \
+            "run abct task_1."
+
+        if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
+            python3 -m rv22 --help
+        fi
+
+        return
+    fi
+
+    if [ "$task" == "task_1" ] ; then
+        python3 -m rv22 \
+            task_1 \
+            ${@:2}
+        return
+    fi
+
+    abcli_log_error "-rv22: $task: command not found."
+}
---
+++ rv22/__init__.py
@@ -0,0 +1,5 @@
+name = "rv22"
+
+version = "1.1.1"
+
+description = "RAW Vancouver PORTAL 2022"
