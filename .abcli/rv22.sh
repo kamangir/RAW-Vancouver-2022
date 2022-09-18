536789bcd4f44c23865053b8492888369e1b2c14
kamangir <arash@kamangir.net>
2022-09-18 12:06:22 -0700

    validating Kanata - kamangir/bolt#709

--- .abcli/rv22.sh
+++ .abcli/rv22.sh
@@ -8,7 +8,7 @@ function RAW_Vancouver_PORTAL_2022() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
-        abcli_help_line "rv22 task_1" \
+        abcli_show_usage "rv22 task_1" \
             "run rv22 task_1."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
